18d377b3f494b501dfecf13e2d59a3391b250200
Igor <igor.golanoff@yandex.ru>
2020-04-06 13:49:05 +0300

    creation of infrastructure. the creation of classes errors

---
+++ libs/loginFailedError.js
@@ -0,0 +1,7 @@
+class LoginFailedError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 401;
+  }
+}
+module.exports = LoginFailedError;
---
+++ models/user.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const validate = require('mongoose-validator');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcryptjs');
